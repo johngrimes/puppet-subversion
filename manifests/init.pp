f50910cf727133b85b1f9d3f9856b989116ebf3e
John Grimes <john@smallspark.com.au>
2014-04-02 11:29:44 +1000

    Disable storage of passwords, and turn on HTTP compression

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,13 @@ class subversion {
   package { 'subversion':
     ensure => 'present'
   }
+
+  file { '/etc/subversion/servers':
+    ensure  => file,
+    source  => 'puppet:///modules/subversion/servers',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '644',
+    require => Package['subversion']
+  }
 }
